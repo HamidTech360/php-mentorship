e367e0aa7235a5b2b8fcb4eea809f2458e87beda
hammedowolabi2001 <68151044+hammedowolabi2001@users.noreply.github.com>
2021-07-16 19:16:06 +0100

    creating an index file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+function myProfile (){
+    echo array("name"=>"Owolabi Hammed",
+                "email"=>"owolabihammed2001@gmail.com", 
+                "hobby"=>"programing", 
+                "Funfact"=>"Easy going nigga");
+}
+
+myProfile()
